9602b0d53d794f07724e19a3a9db73e5052db98e
Sean <ses0384@my.londonmet.ac.uk>
2016-04-17 20:50:46 +0100

    Corrected some UI elements and the location of the PNG file.

--- app/src/main/java/com/project/sean/androidean13/AndroidEAN13Activity.java
+++ app/src/main/java/com/project/sean/androidean13/AndroidEAN13Activity.java
@@ -1,15 +1,23 @@
 package com.project.sean.androidean13;
 
+import android.graphics.Bitmap;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class AndroidEAN13Activity extends AppCompatActivity {
+
+    TextView t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +30,25 @@ public class AndroidEAN13Activity extends AppCompatActivity {
                 createEAN13Code();
             }
         });
+        Button btn2 = (Button)findViewById(R.id.btn_image);
+        btn2.setOnClickListener( new Button.OnClickListener() {
 
-        TextView t = (TextView)findViewById(R.id.barcode);
+            public void onClick(View arg0) {
+                imageEAN13Code();
+            }
+        });
+
+        t = (TextView)findViewById(R.id.barcode);
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/EanP72Tt Normal.Ttf");
         t.setTypeface(font);
         t.setText("$!24J5IH-gbdbah!");
+
+
     }
 
     public void createEAN13Code()
     {
-
-        EditText e = (EditText)findViewById(R.id.edittext1);
+        EditText e = (EditText)findViewById(R.id.etBarcode);
         String s = e.getText().toString();
         if( s == null || s.length() != 12 ) {
             Toast.makeText(AndroidEAN13Activity.this, "A barcode requires 12 numbers.", Toast.LENGTH_SHORT)
@@ -47,4 +63,35 @@ public class AndroidEAN13Activity extends AppCompatActivity {
         Toast.makeText(AndroidEAN13Activity.this, "Barcode generated successfully!", Toast.LENGTH_SHORT)
                 .show();
     }
+
+    /**
+     * Converts the TextView into a Bitmap so that it can be stored as a png file.
+     */
+    public void imageEAN13Code() {
+        t.setDrawingCacheEnabled(true); // Enable drawing cache before calling the getDrawingCache() method
+        // Get bitmap object from the TextView
+        Bitmap tvImage= Bitmap.createBitmap(t.getDrawingCache());
+
+        String filename = "tvimage.png";
+
+        System.out.print(getFilesDir());
+        FileOutputStream outputStream = null;
+        try {
+             outputStream = openFileOutput(filename, MODE_WORLD_READABLE);
+            // Save the bitmap object to file
+            tvImage.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException ioex) {
+                    Log.d("IOException", ioex.toString());
+                }
+            }
+        }
+    }
+
 }
